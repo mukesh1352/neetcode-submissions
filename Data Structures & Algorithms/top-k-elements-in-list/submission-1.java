0f38fe8c619a907316747c3d9c8b1a4735be8222
mukesh <140415041+mukesh1352@users.noreply.github.com>
2026-06-23 07:52:29 +0530

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer,Integer> mp = new HashMap<>();
+
+        for(int num : nums)
+            mp.put(num, mp.getOrDefault(num, 0) + 1);
+
+        PriorityQueue<Integer> pq =
+            new PriorityQueue<>((a,b) -> mp.get(b) - mp.get(a));
+
+        for(int key : mp.keySet())
+            pq.add(key);
+
+        int[] ans = new int[k];
+
+        for(int i = 0; i < k; i++)
+            ans[i] = pq.poll();
+
+        return ans;
+    }
+}
